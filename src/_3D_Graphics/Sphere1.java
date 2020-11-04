fa35a31f77dc9d46a96228e3e96372149415858e
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-11-04 20:39:40 +0000

    ...

--- src/_3D_Graphics/Sphere1.java
+++ src/_3D_Graphics/Sphere1.java
@@ -5,11 +5,15 @@ package _3D_Graphics;
  * @author Fenias
  */
 import javafx.application.*;
+import javafx.event.*;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
+import javax.swing.*;
 
 public class Sphere1 extends Application{
 
@@ -24,6 +28,10 @@ public class Sphere1 extends Application{
     @Override
     public void start(Stage myStage) throws Exception{
         
+        myStage.setOnCloseRequest(event -> {
+            System.out.println("Closing the Program!");
+        });
+        
         // giving the Stage a title
         myStage.setTitle("Sphere");
         
@@ -36,13 +44,28 @@ public class Sphere1 extends Application{
         // set a Scene on the Stage
         Scene myScene = new Scene(group, width, height);
         
+        Camera camera = new PerspectiveCamera();
+        
+        myScene.setCamera(camera);
+        
+        // setting a new color to the Scene
+        myScene.setFill(Color.SILVER);
+        
         // redirecting the sphere to the center
         sphere.translateXProperty().set(width / 2);
         sphere.translateYProperty().set(height / 2);
         
+        sphere.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+            if (event.getCode() == KeyCode.A){
+                sphere.translateZProperty().set(sphere.getTranslateZ() + 10);
+            }
+            else if (event.getCode() == KeyCode.B)
+                sphere.translateZProperty().set(sphere.getTranslateZ() - 10);
+        });
+        
         // setting the the Scene on the Stage
         myStage.setScene(myScene);
-        
+
         // showing the Stage and it's Scene
         myStage.show();
     }
