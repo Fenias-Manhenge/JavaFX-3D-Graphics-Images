fe5a2c1ed21d7bd2d184297e1bf28bb69ae502a0
Fenias-Manhenge <53791616+Fenias-Manhenge@users.noreply.github.com>
2020-11-09 23:47:13 +0000

    Class Transform3D!
    able to rotate the objects through the exis!

--- src/_3D_Graphics/Transform3D.java
+++ src/_3D_Graphics/Transform3D.java
@@ -10,4 +10,29 @@ import javafx.scene.transform.*;
 
 public class Transform3D extends Group{
     
+    Rotate r;
+    Transform t = new Rotate();
+    
+    // method that will rotate the object on the X axis
+    void rotateByX(int ang){
+        r = new Rotate(ang, Rotate.X_AXIS);
+        t = t.createConcatenation(r);
+        this.getTransforms().clear();
+        this.getTransforms().add(t);
+    }
+    
+    // method that will rotate the object on the Y axis
+    void rotateByY(int ang){
+        r = new Rotate(ang, Rotate.Y_AXIS);
+        t = t.createConcatenation(r);
+        this.getTransforms().clear();
+        this.getTransforms().add(t);
+    }
+    
+    void rotateByZ(int ang){
+        r = new Rotate(ang, Rotate.Z_AXIS);
+        t = t.createConcatenation(r);
+        this.getTransforms().clear();
+        this.getTransforms().add(t);
+    }
 }
